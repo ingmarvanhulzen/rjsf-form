ac85855b35019ca003537322e5a758fd335b22c8
Ingmar van Hulzen <ivanhulzen@jouwomgeving.nl>
2022-08-17 17:16:14 +0200

    alter templates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "@rjsf/core";
+
+import schema from "./schema.json";
+import uiSchema from "./ui_schema.json";
+
+function StepObjectFieldTemplate({ title, properties }) {
+  const [index, setIndex] = React.useState(0);
+
+  const goBack = React.useCallback(() => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  }, [properties, index, setIndex]);
+
+  const goForward = React.useCallback(() => {
+    if (index < properties.length - 1) {
+      setIndex(index + 1);
+    }
+  }, [properties, index, setIndex]);
+
+  return (
+    <div className="Container">
+      <div className="Heading">
+        <span>{title}</span>
+      </div>
+      <div className="Content">{properties[index].content}</div>
+      <div className="Navigation">
+        <a className="Backward" onClick={goBack}>
+          Vorige
+        </a>
+        <a className="Forward" onClick={goForward}>
+          Volgende
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function ObjectFieldTemplate({ title, properties }) {
+  return (
+    <div className="Container">
+      <div className="Heading">
+        <span>{title}</span>
+      </div>
+      <div className="Content">
+        {properties.map((propertie) => propertie.content)}
+      </div>
+      <div className="Navigation">
+        <a className="Forward">Opslaan</a>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      ObjectFieldTemplate={StepObjectFieldTemplate}
+    />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// {
+//   "title": "A registration form (nullable)",
+//   "description": "A simple form example using nullable types",
+//   "type": "object",
+//   "properties": {
+//     "verlangen": {
+//       "type": "string",
+//       "title": "Heeft u in de afgelopen 12 maanden regelmatig het verlangen gehad om te stoppen met gebruik?",
+//       "enum": ["Ja", "Nee"]
+//     },
+//     "lastName": {
+//       "type": "string",
+//       "title": "Last name"
+//     },
+//     "age": {
+//       "type": "integer",
+//       "title": "Age"
+//     }
+//   }
+// }
